19cba7d13ee7b72a5786f86a2626ced039b82c86
Igor Staszak <istaszak@edu.cdv.pl>
2019-04-15 16:29:56 +0200

    drugie zajecia

--- programowanie-strukturalne/3_math_random.py
+++ programowanie-strukturalne/3_math_random.py
@@ -8,3 +8,13 @@ print(pi)
 #pierwiastek
 pierwiastek = math.sqrt(9)
 print(pierwiastek)
+
+#moduł random
+import random
+losuj = random.random()
+print(f'Zmienna wylosowana: {losuj}')
+
+losujZlisty = random.choice([1, 2, 3, 4])
+print(f'Wylosowana zmienna z listy [1-4]: {losujZlisty}')
+
+print()
